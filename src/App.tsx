0bbd518e6b1be106cbe3f3b330bc500d717c35db
viet34tqc <viet34tqc@gmail.com>
2021-11-02 21:34:40 +0700

    scale

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import { select, Selection } from 'd3';
+import { scaleBand, scaleLinear, select, Selection } from 'd3';
+import { max } from 'd3-array';
 import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 const data = [
-	{ width: 100, height: 200, color: 'blue' },
-	{ width: 100, height: 150, color: 'red' },
-	{ width: 100, height: 120, color: 'green' },
-	{ width: 100, height: 100, color: 'orange' },
-	{ width: 100, height: 70, color: 'teal' },
+	{ name: 'foo', number: 9000 },
+	{ name: 'bar', number: 1234 },
+	{ name: 'baz', number: 3456 },
+	{ name: 'fuu', number: 4567 },
+	{ name: 'pill', number: 5678 },
 ];
 
 function App() {
@@ -18,6 +19,15 @@ function App() {
 		null,
 		undefined
 	>>(null);
+	const maxValue = max(data, d => d.number) || 10000;
+	const y = scaleLinear()
+		//domain([0, 10000]) 10000 is the biggest number above, and 500 is the height of the svg. However, the data can be remove or changed, so does the max number
+		.domain([0, maxValue])
+		.range([0, 500]);
+	const x = scaleBand()
+		.domain(data.map(d => d.name))
+		.range([0, 800])
+		.paddingInner(0.05);
 	useEffect(() => {
 		if (!selection) {
 			setSelection(select(svgRef.current));
@@ -27,17 +37,16 @@ function App() {
 				.data(data)
 				.enter()
 				.append('rect')
-				.attr('width', (d) => d.width)
-				.attr('height', (d) => d.height)
-				.attr('fill', (d) => d.color)
-				.attr('x', (_, i) => i * 100);
+				.attr('width', x.bandwidth)
+				.attr('height', d => y(d.number))
+				.attr('x', d => x(d.name) || null)
+				.attr('fill', 'blue');
 		}
-	}, [selection]);
+	}, [selection, y, x]);
+
 	return (
 		<div>
-			<svg ref={svgRef} width={500}>
-
-			</svg>
+			<svg ref={svgRef} width={800} height={500} />
 		</div>
 	);
 }
