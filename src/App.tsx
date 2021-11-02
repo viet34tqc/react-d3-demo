42434e86f7cfbafcf528ef631db4c577b69b8c7a
viet34tqc <viet34tqc@gmail.com>
2021-11-02 15:48:46 +0700

    Data join

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import { select, Selection } from 'd3';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
+const data = [
+	{ width: 100, height: 200, color: 'blue' },
+	{ width: 100, height: 150, color: 'red' },
+	{ width: 100, height: 120, color: 'green' },
+	{ width: 100, height: 100, color: 'orange' },
+	{ width: 100, height: 70, color: 'teal' },
+];
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const svgRef = useRef(null);
+	const [selection, setSelection] = useState<null | Selection<
+		null,
+		unknown,
+		null,
+		undefined
+	>>(null);
+	useEffect(() => {
+		if (!selection) {
+			setSelection(select(svgRef.current));
+		} else {
+			selection
+				.selectAll('rect')
+				.data(data)
+				.enter()
+				.append('rect')
+				.attr('width', (d) => d.width)
+				.attr('height', (d) => d.height)
+				.attr('fill', (d) => d.color)
+				.attr('x', (_, i) => i * 100);
+		}
+	}, [selection]);
+	return (
+		<div>
+			<svg ref={svgRef} width={500}>
+
+			</svg>
+		</div>
+	);
 }
 
 export default App;
